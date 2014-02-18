5d87fb857a70adecff88179797624179a990bd30
Abhi Ramesh <abhiramesh@gmail.com>
2014-02-18 14:14:04 -0500

    setup git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Debate::Application.config.secret_token = '400fc2e41962a001eb76791d53207d5b243142e113462478844c0292ffa0fbe50b258ce67728bda3b71af08641e2cb98403d7ead4a3c47f4a8223f5eeb1801d8'
